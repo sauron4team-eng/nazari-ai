0f4b58e5d6b52d46441bd353bdc6963b0ab46f4c
AbakarSilet <155161891+AbakarSilet@users.noreply.github.com>
2026-07-21 13:57:54 +0100

    Revert "Tasks/menu"

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'screens/splash_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/documents_screen.dart';
-import 'screens/ai_assistant_screen.dart';
-import 'screens/study_tools_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-  runApp(const NazariAIApp());
+  runApp(const MyApp());
 }
 
-class NazariAIApp extends StatelessWidget {
-  const NazariAIApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NazariAI',
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF0B5D3B),
-          primary: const Color(0xFF0B5D3B),
-          secondary: const Color(0xFFD4A017),
-          surface: const Color(0xFFFFFFFF),
-          //background: const Color(0xFFF9F7F2),
-          onSurface: const Color(0xFF1F2937),
-        ),
-        scaffoldBackgroundColor: const Color(0xFFF9F7F2),
-        textTheme: GoogleFonts.hankenGroteskTextTheme(),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Colors.white,
-          selectedItemColor: Color(0xFF0B5D3B),
-          unselectedItemColor: Color(0xFF6B7280),
-          type: BottomNavigationBarType.fixed,
-          elevation: 8,
-          selectedLabelStyle: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
-          ),
-          unselectedLabelStyle: TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
-      home: const SplashScreen(),
+      home: const Home(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  List<Widget> get _screens => [
-    const HomeScreen(),
-    const DocumentsScreen(),
-    const AiAssistantScreen(),
-    StudyToolsScreen(onNavigateToTab: _onItemTapped),
-  ];
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(index: _selectedIndex, children: _screens),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.description_outlined),
-            activeIcon: Icon(Icons.description),
-            label: 'Documents',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.psychology_outlined),
-            activeIcon: Icon(Icons.psychology),
-            label: 'AI Assistant',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics_outlined),
-            activeIcon: Icon(Icons.analytics),
-            label: 'Study Tools',
-          ),
-        ],
-      ),
-    );
+    return const Scaffold(body: Center(child: Text('Initialization...')));
   }
 }
